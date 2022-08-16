563e6b761a5b8bfe194b8bced33a78ce93279ca2
Shad Kaske <shadkaske@gmail.com>
2022-08-16 14:43:47 -0500

    more options

--- init.lua
+++ init.lua
@@ -17,7 +17,25 @@ require("onedark").load()
 -- Options
 vim.opt.number = true
 vim.opt.relativenumber = true
+-- Spaces not tabs
+vim.opt.smarttab = true
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.mouse = 'a'
+vim.opt.timeoutlen = 300
+vim.opt.hidden = true
+vim.opt.cmdheight = 1
+vim.opt.title = true
+vim.opt.scrolloff = 3
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.lazyredraw = true
+
+vim.opt.errorbells = false
+vim.opt.visualbell = true
