822c7c9c8785bcb53cee9f733ce93869245ae94f
Shad Kaske <shadkaske@gmail.com>
2022-08-17 11:55:47 -0500

    Bufferline Offsets

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,1 +1,34 @@
-require("bufferline").setup{}
+require("bufferline").setup({
+  options = {
+    offsets = {
+      {
+        filetype = "undotree",
+        text = "Undotree",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      {
+        filetype = "NvimTree",
+        text = "Explorer",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      { filetype = "DiffviewFiles",
+        text = "Diff View",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+      {
+        filetype = "flutterToolsOutline",
+        text = "Flutter Outline",
+        highlight = "PanelHeading",
+      },
+      {
+        filetype = "packer",
+        text = "Packer",
+        highlight = "PanelHeading",
+        padding = 1,
+      },
+    },
+  },
+})
