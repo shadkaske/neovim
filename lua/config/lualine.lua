461fea4c4fa442e3195343c88c1d1a4a4ed153ad
Shad Kaske <shadkaske@gmail.com>
2022-08-17 12:58:15 -0500

    Minor LuaLine Update

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -5,13 +5,11 @@ require('lualine').setup {
     section_separators = { left = ' ', right = ' '},
   },
   sections = {
-    lualine_a = {
-      {
-        'mode',
-        fmt = function (str)
-          return str:sub(1,1)
-        end
-      }
-    }
-  }
+    lualine_a = { { 'mode', fmt = function (str) return str:sub(1,1) end } },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
 }
