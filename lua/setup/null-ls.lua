a5a1f39c908d186e1bd68755b678eedad055a275
Shad Kaske <shadkaske@gmail.com>
2022-08-19 09:04:21 -0500

    Null Ls Configuration

--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -8,13 +8,7 @@ nls.setup({
     nls.builtins.formatting.prettier.with({
       extra_args = { "--single-quote", "false" },
     }),
-    nls.builtins.formatting.terraform_fmt,
     nls.builtins.formatting.black,
-    nls.builtins.formatting.goimports,
-    nls.builtins.formatting.gofumpt,
-    nls.builtins.formatting.latexindent.with({
-      extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
-    }),
     nls.builtins.code_actions.shellcheck,
     nls.builtins.diagnostics.vale,
   },
